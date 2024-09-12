46b4bdfb4fe607fd6f5aaff5cfe6e5ffc87acd56
unknown <rodrigovidalalbuquerque.13@gmail.com>
2024-09-12 00:18:17 -0300

    Project Created

---
+++ backend/src/test/java/com/project/deliveryproject/DeliveryprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.deliveryproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
